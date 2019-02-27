d23192d54d29af072e5e8201722034a633e16ef4
Dean <deanhume@gmail.com>
2019-02-27 10:07:17 +0000

    Added base files

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const request = require('request');
 const port = 3000;
 
 app.get('/', function(req, res) {
-    const url = 'https://upload.wikimedia.org/wikipedia/commons/e/e8/View_from_eiffel_tower_2nd_level.jpg';
+    const url = 'json-response.json';
 
     request.get(url).pipe(res);
 });
