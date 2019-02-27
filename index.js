f356b6041b0eef3c0693ea2a8f126deac253d12d
Dean <deanhume@gmail.com>
2019-02-27 11:48:52 +0000

    hacking

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const request = require('request');
 const port = 3000;
+const path = require('path');
 
 app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.get('/request', function(req, res) {
     const url = 'https://raw.githubusercontent.com/deanhume/streams/master/json-response.json';
 
     request.get(url).pipe(res);
